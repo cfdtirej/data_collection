6780ad97fbc85cb61d712d7497c5895f34f43769
cfdtirej <53041956+cfdtirej@users.noreply.github.com>
2021-04-23 18:09:20 +0900

    fix

--- src/kisyocho_scrape/get_past_weather.py
+++ src/kisyocho_scrape/get_past_weather.py
@@ -4,7 +4,7 @@ import os
 import time
 from datetime import datetime
 from pathlib import Path
-from typing import List, Dict
+from typing import List, Dict, Union
 
 import pytz
 import requests
@@ -16,6 +16,25 @@ import settings
 import schemas
 
 
+def get_past_weather_table(area: str, year_: str, month_: str, day_: str) -> List[str]:
+    data_table = [settings.wheater_tb_columns]
+    if not area in settings.place46_no.keys():
+        raise f'{area}以外の地域を選択してください'
+    area_numbers: schemas.PlaceNo = settings.place46_no[area]
+    prec_no, block_no = area_numbers['prec_no'], area_numbers['block_no']
+    url = f'https://www.data.jma.go.jp/obd/stats/etrn/view/hourly_s1.php?prec_no={prec_no}&block_no=47605&year={year_}&month={month_}&day={day_}&view='
+    
+    # request to kishocho
+    response = requests.get(url)
+    response.encoding = response.apparent_encoding
+    soup = BeautifulSoup(response.text)
+    for tr in soup.findAll('tr', class_='mtx'):
+        td = tr.findAll('td')
+        row = [t.string for t in td]
+        if len(row):
+            data_table.append(row)
+    return data_table
+
 _year = '2002'
 _month = '2'
 _day = '1'
@@ -24,10 +43,20 @@ past_url = f'https://www.data.jma.go.jp/obd/stats/etrn/view/hourly_s1.php?prec_n
 res = requests.get(past_url)
 res.encoding = res.apparent_encoding
 
-soup = BeautifulSoup(res.text, 'html.parser')
-table_text = soup.find('table').get_text()
-print(table_text)
+soup = BeautifulSoup(res.text)
+table_rows = soup.findAll('tr',class_='mtx')
+print(table_rows)
+data_table = [settings.wheater_tb_columns]
+for tr in table_rows:
+    td = tr.findAll('td')
+    img = [[i['src'], i['alt']] for i in tr.findAll('img')]
+    
+    # print(td)
+    print(img,alt)
+    if data:
+        data_table.append(data)
 
 
+# print(data_table)
 if __name__ == '__main__':
     pass
--- src/kisyocho_scrape/settings.py
+++ src/kisyocho_scrape/settings.py
@@ -10,4 +10,7 @@ place46_no_jf = Path(__file__).parent / 'place46_no.json'
 with open(place46_no_jf, 'r') as f:
     place46_no: Dict[str, schemas.PlaceNo] = json.load(f)
 
-columns: List[str] = ['時', '気圧', '海面気圧','降水量','気温','露点温度','蒸気圧','湿度','風速','風向','日照時間','全天日射量','降雪','積雪','天気','雲量','視程']
+wheater_tb_columns: List[str] = [
+    '時', '気圧', '海面気圧','降水量', '気温', '露点温度', '蒸気圧', '湿度',
+    '風速', '風向', '日照時間', '全天日射量', '降雪', '積雪', '天気', '雲量', '視程']
+
