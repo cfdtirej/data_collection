b13bc411477d110b6ad42e6893ee0e8cd877922d
cfdtirej <53041956+cfdtirej@users.noreply.github.com>
2021-04-24 13:54:31 +0900

    fix url fmt

--- src/kisyocho_scrape/get_past_weather.py
+++ src/kisyocho_scrape/get_past_weather.py
@@ -2,7 +2,7 @@ import csv
 import json
 import os
 import time
-from datetime import datetime
+from datetime import datetime, timedelta
 from pathlib import Path
 from typing import List, Dict, Union
 
@@ -16,6 +16,8 @@ import settings
 import schemas
 
 
+tz_jst = pytz.timezone('Asia/Tokyo')
+
 def get_past_weather_table(area: str, year_: str, month_: str, day_: str) -> List[str]:
     if not area in settings.place46_no.keys():
         raise f'{area}以外の地域を選択してください'
@@ -24,7 +26,7 @@ def get_past_weather_table(area: str, year_: str, month_: str, day_: str) -> Lis
     block_no: int = area_numbers['block_no']
     
     # request to kishocho
-    url: str = f'https://www.data.jma.go.jp/obd/stats/etrn/view/hourly_s1.php?prec_no={prec_no}&block_no=47605&year={year_}&month={month_}&day={day_}&view='
+    url: str = f'https://www.data.jma.go.jp/obd/stats/etrn/view/hourly_s1.php?prec_no={prec_no}&block_no={block_no}&year={year_}&month={month_}&day={day_}&view=p1'
     response = requests.get(url)
     response.encoding = response.apparent_encoding
     soup = BeautifulSoup(response.text, 'html.parser')
@@ -49,5 +51,19 @@ def get_past_weather_table(area: str, year_: str, month_: str, day_: str) -> Lis
     return data_table
 
 
+def weather_csv_write_all() -> None:
+    yesterday: datetime = datetime.now() - timedelta(days=1)
+    for ts in pd.date_range('2002-01-01', yesterday.date(), freq='D'):
+        ts: datetime = datetime.strptime(str(ts), '%Y-%m-%d %H:%M:%S').astimezone(tz_jst)
+        for area in settings.place46_no.keys():
+            weather_table = get_past_weather_table(area, ts.year, ts.month, ts.day)
+            print(ts,area,len(weather_table))
+    return
+weather_csv_write_all()
+
+# print(type(yesterday))
+
+
 if __name__ == '__main__':
-    get_past_weather_table('金沢',2020,1,2)
+   data = get_past_weather_table('札幌',2020,1,2)
+   print(data)
