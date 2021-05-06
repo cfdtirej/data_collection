aeebc4e2c501df40f06880b97ae91a70181fc006
cfdtirej <53041956+cfdtirej@users.noreply.github.com>
2021-05-06 13:36:29 +0900

    aa

--- src/tools/csv_to_lineprotocol.py
+++ src/tools/csv_to_lineprotocol.py
@@ -7,14 +7,19 @@ from influxdb import InfluxDBClient
 
 
 def csv_to_lineprotocol(
-    csvfile: Path, database: str, measurement: str, tags: Dict[str, str]
-) -> Iterator[Dict[str: Any]]:
-    df = pd.read_csv(csvfile, col=0)
+    csvfile: Path, database: str, measurement: str, tags: Dict[str, str] = None
+) -> Iterator[Dict[str, Any]]:
+    if not tags:
+        tags = {'measurement': measurement}
+    df = pd.read_csv(csvfile, index_col=0)
     df.index = pd.to_datetime(df.index).tz_localize('Asia/Tokyo')
     for ts, vals in zip(df.index, df.values):
         fields = {}
         for col, val in zip(df.columns, vals):
-            if not np.isnan(val):
+            if type(val) == (float or int):
+                if not np.isnan(val):
+                    fields[col] = val
+            else:
                 fields[col] = val
         lineprotocol: List[Dict[str, Any]] = [{
             'time': ts.isoformat(),
@@ -23,3 +28,10 @@ def csv_to_lineprotocol(
             'fields': fields
         }]
         yield lineprotocol
+
+if __name__ == '__main__':
+    csvfiles = Path(__file__).parents[1] / 'kisyocho_scrape' / 'past_weather'
+    csvfiles = csvfiles.glob('**/*.csv')
+    for csvfile in csvfiles:
+        for lineprotocol in csv_to_lineprotocol(csvfile=csvfile,measurement='hello', database='db'):
+            print(lineprotocol)
